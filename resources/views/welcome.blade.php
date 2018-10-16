61d8292f13b28fd57de61b88153fe8a912b25237
shanwazfarooqe <42293694+shanwazfarooqe@users.noreply.github.com>
2018-10-16 11:57:04 +0530

    updated
    heading changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Shanavas Changed again
+                    Opera
                 </div>
 
                 <div class="links">
